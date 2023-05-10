213cab2b76705f2823cf96a10418428ebeb651a5
abdelghni-hamanar <abdelghni.hamanar@gmail.com>
2023-05-10 10:33:21 -0700

    base prototype for our data

--- shell.h
+++ shell.h
@@ -14,22 +14,83 @@
 #include <fcntl.h>
 #include <errno.h>
 
+#define BUFSIZE 1024
+#define TOK_BUFSIZE 128
+#define TOK_DELIM " \t\r\n\a"
 
-extern char **environ
+extern char **environ;
 
-#define BUFFER_SIZE 1024
+/**
+ * struct data - struct contains all relevant data
+ * @av: argument
+ * @input: command line entred by user
+ * @args: token of command line
+ * @status: last status of shell
+ * @counter: lines counter
+ * @_environ: env variable
+ * @pid: child process
+ */
 
-ssize_t __getline(char **, size_t *, FILE *);
+typedef struct data
+{
+	char **av;
+	char *input;
+	char **args;
+	int status;
+	int counter;
+	char **_environ;
+	char *pid;
+} data_shell;
 
-int set_environment_variable(char **, char *, char *);
-int unset_environment_variable(char **, char *);
+/**
+ * struct sep_list_s - contains linked list signal
+ * @separator: contains ; | &
+ * @next: next node
+ */
+typedef struct sep_list_s
+{
+	char separator;
+	struct sep_list_s *next;
+} sep_list;
 
-/* handle the cd and PWD*/
-void cd_command(char **, char **);
+/**
+ * struct line_list_s - linked list signal
+ * @line: conatins command line
+ * @next: next node
+ */
 
-/*implement some function to reduce size of main function*/
-void setenv_command(char **, char **);
-void unsetenv_command(char **, char **);
-void execute_command(char *, char **);
+typedef struct line_list_s
+{
+	char *line;
+	struct line_list_s *next;
+} line_list;
+
+/**
+ * struct var_list_s - linked list signal
+ * @len_var: len of variable
+ * @val: valueof variable
+ * @len_val: len of value
+ * @next: next node
+ */
+typedef struct var_list_s
+{
+	int len_var;
+	char *val;
+	int len_val;
+	struct var_list_s *next;
+} r_var;
+
+/**
+ * struct builtin_s - struct for builtin
+ * @name: name for command builtin i.e cd exit env etc
+ * @f: data type pointer function
+ */
+
+typedef struct builtin_s
+{
+	char *name;
+	int (*f)(data_shell *datash);
+} builtin_t;
+
 
 #endif
